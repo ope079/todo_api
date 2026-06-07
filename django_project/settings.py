bdaa32e4226dfcb9bb76bcbbbc9f572e4053370a
ope079 <o_orekoya@yahoo.com>
2026-06-07 09:27:12 +0100

    add keep to static

--- django_project/settings.py
+++ django_project/settings.py
@@ -138,6 +138,6 @@ REST_FRAMEWORK = {
 
 CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
 
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("staticfiles"))]
+STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
 STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
